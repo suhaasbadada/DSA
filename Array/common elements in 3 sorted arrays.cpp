b9b22753869fb4d4b9b3f3c8de495b2ddbb9e7c2
suhaasbadada <suhaasbadada@gmail.com>
2020-11-27 18:47:49 +0530

    Update common elements in 3 sorted arrays.cpp

--- Array/common elements in 3 sorted arrays.cpp
+++ Array/common elements in 3 sorted arrays.cpp
@@ -29,7 +29,7 @@ void CommonElements(int arr1[],int arr2[],int arr3[],int n1,int n2,int n3){
 int main(){
 
 
-	int arr1[]={1, 5, 10, 20, 40, 80}; 
+    int arr1[]={1, 5, 10, 20, 40, 80}; 
     int arr2[]={6, 7, 20, 80, 100}; 
     int arr3[]={3, 4, 15, 20, 30, 70, 80, 120}; 
     int n1 = sizeof(arr1)/sizeof(arr1[0]); 
