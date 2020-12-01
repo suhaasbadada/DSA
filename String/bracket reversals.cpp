26f22c06a90f410159ff26e8536e01cf1e361b72
suhaasbadada <suhaasbadada@gmail.com>
2020-12-01 23:45:16 +0530

    Add files via upload

---
+++ String/bracket reversals.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int CountReversals(string expression){
+	// first step remove balanced part
+	int len=expression.length();
+
+	if(len%2!=0)
+		return -1;
+
+	stack <char> s;
+	int i;
+
+	for(i=0;i<len;i++){
+		if(expression[i]=='}'&&!s.empty()){
+			if(s.top()=='{'){
+				s.pop();
+			}else{
+				s.push(expression[i]);
+			}
+		}else{
+			s.push(expression[i]);
+		}
+	}
+
+	int reducedLen=s.size();
+
+	int n=0,m=0;
+	while(!s.empty()){
+
+		if(s.top()=='{'){
+			n++;
+		}else{
+			m++;
+		}
+
+		s.pop();
+	}
+
+	//cout<<n<<" "<<m;
+
+	//return n/2 + m/2;
+	return (reducedLen/2+n%2);
+
+	// both return statements are same
+
+}
+
+int main(){
+	string expr = "}{{}}{{{"; 
+
+	cout<<CountReversals(expr);
+
+	return 0;
+}
